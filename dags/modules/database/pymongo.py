ae4adac14d6a1c59620db7040d20bf283269bb1d
FacerAin <ywsong.dev@kakao.com>
2023-11-06 13:49:11 +0900

    feat: refactor PymongoClient to handle connection errors

--- dags/modules/database/pymongo.py
+++ dags/modules/database/pymongo.py
@@ -5,12 +5,17 @@ from dags.modules.utils import singleton
 
 @singleton
 class PymongoClient:
-    db = None
-    client = None
 
     def __init__(self, host="localhost", port=27017):
-        self.client = MongoClient(host=host, port=port)
+        self.client = self.get_client_connection(host, port)
         self.collection = self.client["khugpt"]["documents"]
+
+    def get_client_connection(self, host, port):
+        try:
+            client = MongoClient(host=host, port=port)
+        except Exception as e:
+            raise f"Can't connect to mongodb host:{host} port:{port}"
+        return client
 
     def get_collection(self):
         return self.collection
