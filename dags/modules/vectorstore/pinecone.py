be6b1a1e98b7e6359d2641760a7d7a74fcc5562b
FacerAin <ywsong.dev@kakao.com>
2023-11-07 11:46:56 +0900

    [#9] feat: improve upsert pinecone using batch async

--- dags/modules/vectorstore/pinecone.py
+++ dags/modules/vectorstore/pinecone.py
@@ -4,11 +4,21 @@ import os
 from dags.modules.utils import singleton
 import openai
 from dotenv import load_dotenv
-from typing import Dict
+from typing import Dict, List, Iterable
 from tqdm import tqdm
+from itertools import islice
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def batch_iterate(size: int, iterable):
+    """Utility batching function."""
+    it = iter(iterable)
+    while True:
+        chunk = list(islice(it, size))
+        if not chunk:
+            return
+        yield chunk
 
 
 @singleton
@@ -32,9 +42,11 @@ class PineconeClient:
             }
         }
 
-    def _get_embedding(self, text: str, model="text-embedding-ada-002"):
-        text = text.replace("\n", " ")
-        return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
+    def _get_embeddings(self, texts: List[str], model="text-embedding-ada-002"):
+        texts = [text.replace("\n", " ") for text in texts]
+        responses = openai.Embedding.create(input = texts, model=model)['data']
+        embeddings = [res['embedding'] for res in responses]
+        return embeddings
 
     def _get_pinecone_index(self, index_name: str, pool_threads: int = 4):
         indexes = pinecone.list_indexes()
@@ -48,9 +60,26 @@ class PineconeClient:
             )
         return index
 
-    def upsert_documents(self, documents):
-        vectors = [self._transform_pinecone_vector(doc) for doc in tqdm(documents)]
-        self.pinecone_index.upsert(vectors)
+    def upsert_documents(self, documents, batch_size: int = 32, embedding_chunk_size: int = 1000):
+        contents = []
+        ids = []
+        metadatas = []
+        for doc in documents:
+            contents.append(doc['page_content'])
+            ids.append(str(doc['_id']))
+            metadatas.append({"page_url": doc["page_url"], 
+                "collected_at": doc["collected_at"]})
+            
+        for i in range(0, len(documents), embedding_chunk_size):
+            chunk_contents = contents[i:i+embedding_chunk_size]
+            chunk_ids = ids[i:i+embedding_chunk_size]
+            chunk_metadatas = metadatas[i:i+embedding_chunk_size]
+            embeddings = self._get_embeddings(chunk_contents)
+            async_res = [
+                self.pinecone_index.upsert(batch, async_req=True)
+                for batch in batch_iterate(batch_size, zip(chunk_ids, embeddings, chunk_metadatas))
+            ]
+            [res.get() for res in async_res]
 
 
 
