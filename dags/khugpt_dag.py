80dc56cf5f92c2860c83074c275000ba646d6b28
FacerAin <ywsong.dev@kakao.com>
2023-12-05 00:48:59 +0900

    feat: update airflow nodes

--- dags/khugpt_dag.py
+++ dags/khugpt_dag.py
@@ -16,17 +16,24 @@
 # specific language governing permissions and limitations
 # under the License.
 
-"""
-### Tutorial Documentation
-Documentation that goes along with the Airflow tutorial located
-[here](https://airflow.apache.org/tutorial.html)
-"""
-# [START tutorial]
 # [START import_module]
-from datetime import timedelta
+from dotenv import load_dotenv
+from datetime import timedelta, date
+import os
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
+from dags.modules.collect.swcon import SwconCollector
+from dags.modules.collect.swedu import SweduCollector
+from dags.modules.collect.khutoday import KhutodayCollector
+from dags.modules.database.pymongo import PymongoClient
+from dags.modules.vectorstore.pinecone import PineconeClient
+
+load_dotenv(verbose=True)
+
+PINECONE_API_KEY=os.environ['PINECONE_API_KEY']
+PINECONE_ENVIRONMENT_REGION=os.environ['PINECONE_ENVIRONMENT_REGION']
+OPENAI_API_KEY=os.environ['OPENAI_API_KEY']
 
 # Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
@@ -73,30 +80,68 @@ dag = DAG(
 )
 # [END instantiate_dag]
 
-# t1, t2 and t3 are examples of tasks created by instantiating operators
 # [START basic_task]
 
 
 def print_test(context):
     print(f"test: {context}")
 
+def collector_khu_today(context: str):
+    assert context == "khu_today"
+    collector = KhutodayCollector
+    docs = collector.collect()
+    if docs:
+        collector.upload_db()
+    print(f"collect {context}: {len(docs)} documents at {str(date.today())}")
+
+def collector_swedu(context: str) -> None:
+    assert context == "swedu"
+    collector = SweduCollector()
+    docs = collector.collect()
+    if docs:
+        collector.upload_db()
+    print(f"collect {context}: {len(docs)} documents at {str(date.today())}")
+
+def collector_swcon(context: str) -> None:
+    assert context == "swcon"
+    collector = SwconCollector()
+    docs = collector.collect()
+    if docs:
+        collector.upload_db()
+    print(f"collect {context}: {len(docs)} documents at {str(date.today())}")
+
+def transformer_vector(context: str) -> None:
+    assert context == "vector"
+    client = PymongoClient()
+    pinecone_client = PineconeClient(index_name="khugpt", pinecone_api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT_REGION, openai_api_key=OPENAI_API_KEY)
+    collection = client["khugpt"]["documents"]
+    res = collection.find({})
+    docs = []
+    for doc in res:
+        docs.append(doc)
+    
+    if docs:
+        pinecone_client.upsert_documents(docs)
+
+    print(f"transform & upload {context}: {len(docs)} documents at {str(date.today())}")
+
 
 collect_khu_today = PythonOperator(
     task_id="collect_khu_today",
-    python_callable=print_test,
+    python_callable=collector_khu_today,
     op_kwargs={"context": "khu_today"},
     dag=dag,
 )
-collect_example1 = PythonOperator(
+collect_swedu = PythonOperator(
     task_id="collect_swedu",
-    python_callable=print_test,
-    op_kwargs={"context": "collect_swedu"},
+    python_callable=collector_swedu,
+    op_kwargs={"context": "swedu"},
     dag=dag,
 )
-collect_example2 = PythonOperator(
-    task_id="collect_example2",
-    python_callable=print_test,
-    op_kwargs={"context": "collect_example1"},
+collect_swcon = PythonOperator(
+    task_id="collect_swcon",
+    python_callable=collector_swcon,
+    op_kwargs={"context": "swcon"},
     dag=dag,
 )
 
@@ -109,7 +154,7 @@ aggregate_data = PythonOperator(
 
 transform_vector = PythonOperator(
     task_id="transform_vector",
-    python_callable=print_test,
+    python_callable=transformer_vector,
     op_kwargs={"context": "vector"},
     dag=dag,
 )
@@ -122,9 +167,8 @@ report = PythonOperator(
 )
 
 (
-    [collect_khu_today, collect_example1, collect_example2]
+    [collect_khu_today, collect_swcon, collect_swedu]
     >> aggregate_data
     >> transform_vector
     >> report
 )
-# [END tutorial]
